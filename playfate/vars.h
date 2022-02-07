2764d06f1bb93aac7c64161b8e0614e9a315bb1a
EricOakford <eoakford@live.com>
2022-02-07 13:51:36 -0500

    FATEDEMO, PLAYFATE: New decompilations
    I now use NUTcracker's decompiler for the v5-v8 games., which outputs code that more closely matches the original SCUMM syntax.
    I started with the Fate of Atlantis demos.

---
+++ playfate/vars.h
@@ -0,0 +1,31 @@
+// Actors
+1 = Indy
+2 = Sophia
+3 = Orichalcum Detector
+
+// Verbs
+1 = Box o' verbs
+2 = Box o' items
+3 = Open
+4 = Close
+5 = Give
+6 = Push
+7 = Pull
+8 = Use
+9 = Look at
+10 = Walk to
+11 = Pick up
+12 = Talk to
+53 = Path (Team, Fists, or Wits)
+54 = Current IQ Points
+55 = Total IQ Points
+
+// Variables
+238 = Current path
+	0 = None
+	1 = Fists
+	2 = Wits
+	3 = Team
+
+// Bit flags
+650 = Is Demo
