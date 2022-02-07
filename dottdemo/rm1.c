3f128ab174821cdeb180e17a96acdbcab0c91f3b
EricOakford <eoakford@live.com>
2022-02-07 16:16:24 -0500

    DOTTDEMO, SAMDEMO: New decompilations
    And now here's new decompilations for the rolling demos of Day of the Tentacle and Sam & Max Hit the Road!
    
    With DOTT, I was able to get the original room names from using DFExplorer with DOTT Remastered.

--- dottdemo/rm1.c
+++
@@ -1,7 +0,0 @@
-// EN
-[0000] (65) stopObjectCodeA()
-END
-
-// EX
-[0000] (65) stopObjectCodeA()
-END
---
+++ dottdemo/vars.h
@@ -0,0 +1,17 @@
+// Actors
+1 = Bernard
+2 = Laverne
+3 = Hoagie
+
+// Verbs
+1 = Box 'o Verbs
+2 = Open
+3 = Close
+4 = Give
+5 = Push
+6 = Pull
+7 = Use
+8 = Look At
+9 = Pick up
+10 = Talk to
+11 = Walk to
