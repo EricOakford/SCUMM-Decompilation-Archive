0b2b69102c5f41112e512cf6f7acdafcedfbf102
EricOakford <eoakford@live.com>
2022-02-09 19:55:57 -0500

    ATLANTIS: Decompilation in progress
    It's a big one. I haven't gone through and identified everything I could yet. But it'll still be useful for reference.

---
+++ atlantis/vars.h
@@ -0,0 +1,63 @@
+// Actors
+1 = Indy
+2 = Sophia
+3 = Marcus
+4 = Kerner
+5 = Biff
+
+// Verbs
+1 = Box-o-verbs
+2 = Box-o-items
+3 = Open
+4 = Close
+5 = Give
+6 = Push
+7 = Pull
+8 = Use
+9 = Look-at
+10 = Walk-to
+11 = Pick-up
+12 = Talk-to
+53 = Path (Team, Fists, or Wits)
+54 = Current-IQ-Points
+55 = Total-IQ-Points
+
+// Variables
+209 = random-message
+211 = shelf-look-count
+224 = prev-room
+229 = bead-count
+
+233 = collection-name
+238 = current-path
+	0 = None
+	1 = Fists
+	2 = Wits
+	3 = Team
+240 = correct-atlantis-posn
+241 = correct-sunstone-posn
+242 = correct-moonstone-posn
+243 = correct-worldstone-posn
+313 = magazine-read-count
+424 = beads-in-dish-count
+442 = random-message2
+
+// Bit flags
+17 = necklace-destroyed
+19 = skipped-cutscene
+69 = after-intro
+77 = been-in-indys-office
+78 = been-in-theater
+85 = beaten-by-biff
+210 = found-lost-dialogue
+213 = opened-ice-box
+214 = looked-at-mayonnaise
+216 = hurt-self-with-arrowhead
+217 = lost-dialogue-in-chest
+218 = lost-dialog-in-bookshelf
+219 = lost-dialogue-in-wax-cat
+540 = beat-fritz
+650 = is-demo
+
+// Strings
+39 = Collection-name-str
