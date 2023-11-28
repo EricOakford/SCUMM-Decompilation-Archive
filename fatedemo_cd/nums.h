336e15c3c257266ce02515de332119f5715e5a4d
EricOakford <eoakford@live.com>
2023-11-28 13:04:41 -0500

    FATEDEMO_CD: All done for now
    And now the CD demo is done. Naturally NUTCracker's decompiler doesn't work anymore at the moment, so that's all for now.

---
+++ fatedemo_cd/nums.h
@@ -0,0 +1,195 @@
+game
+{
+    max-actor = 12
+    system-variables = 100
+}
+
+rooms {
+    college-office                                           =     1
+    college-hall                                             =     2
+    college-basement                                         =     3
+    college-attic                                            =     4
+    college-storage                                          =     5
+    college-archive                                          =     6
+    college-catroom                                          =     7
+    cu-passport                                              =     8
+    cu-national-archaeology                                  =     9
+    new-york-street                                          =    10
+    theater-stage                                            =    11
+    theater-wing                                             =    12
+    iceland-exterior                                         =    13
+    digger-top                                               =    14
+    sophias-study                                            =    15
+    atl2-skeletons                                           =    16
+    atl2-digger                                              =    17
+    atl2-double-door                                         =    18
+    azores                                                   =    19
+    desert-over                                              =    20
+    atl3-bridge-to-god-machine                               =    21
+    atl3-god-machine                                         =    22
+    atl3-center                                              =    23
+    deadroom                                                 =    24
+    sub-volcano                                              =    25
+    iceland-interior                                         =    26
+    montecarlo-hotel                                         =    27
+    montecarlo-seance                                        =    28
+    algiers-market                                           =    29
+    atl3-god-machine-high                                    =    30
+    atl2-cu-robot-chest-plate                                =    31
+    desert                                                   =    32
+    labyrinth-exterior                                       =    33
+    atl1-2-bot                                               =    35
+    atl1-orichalcum-machine                                  =    36
+    atl1-orichalcum-pool                                     =    37
+    sub-interior-1                                           =    39
+    sub-interior-2                                           =    40
+    sub-conning-tower                                        =    41
+    salvage-boat                                             =    42
+    crete-ruins-exterior                                     =    43
+    crete-ruins-interior                                     =    44
+    digger-ride                                              =    45
+    atl1-canal                                               =    46
+    cu-ubermann                                              =    47
+    atl1-entrance                                            =    48
+    thera-dock                                               =    49
+    labyrinth                                                =    50
+    catacombs                                                =    51
+    labyrinth-waterfall-1                                    =    52
+    labyrinth-subway                                         =    53
+    labyrinth-hide                                           =    54
+    atl3-mazeroom                                            =    55
+    labyrinth-elevator                                       =    56
+    labyrinth-three                                          =    57
+    crete-transit-view                                       =    58
+    labyrinth-maproom                                        =    59
+    labyrinth-goldbox                                        =    60
+    labyrinth-elevator-top                                   =    61
+    nazi-laboratory                                          =    62
+    thera-landscape                                          =    63
+    algiers-store                                            =    64
+    algiers-omars-house                                      =    65
+    sub-underwater                                           =    66
+    algiers-dig-exterior                                     =    67
+    logo                                                     =    68
+    thera-dig-exterior                                       =    69
+    montecarlo-chase                                         =    70
+    montecarlo-smashup                                       =    71
+    thera-dig-interior                                       =    72
+    algiers-dig-interior                                     =    73
+    cu-microtaur                                             =    74
+    world-map                                                =    75
+    yucatan-exterior                                         =    76
+    yucatan-interior                                         =    77
+    algiers-over                                             =    78
+    cu-necklace                                              =    79
+    algiers-roof                                             =    80
+    lockrock                                                 =    81
+    salvage-boat-underwater                                  =    82
+    cu-plato                                                 =    83
+    cu-gate-diagram                                          =    84
+    atlantis-maze-nw                                         =    85
+    atlantis-maze-ne                                         =    86
+    atlantis-maze-sw                                         =    87
+    atlantis-maze-se                                         =    88
+    end-volcano                                              =    89
+    end-v2                                                   =    90
+    atl2-entrance-lava                                       =    91
+    atl-generic-1                                            =    92
+    atl-generic-2                                            =    93
+    atl2-hallway                                             =    94
+    sea-over                                                 =    95
+    endscene                                                 =    96
+    atl1-cageroom                                            =    97
+    icons                                                    =    98
+}
+
+labels {
+    logo {
+        check-again                     = 20919
+        reset-the-sentence              = 21116
+        pick-up-current-noun1           = 21550
+        too-far-to-reach                = 21795
+        immediate-execute               = 21836
+        done-sentence                   = 21995
+        
+        default-pull                    = 22125
+        default-wont-work               = 22418
+        said-default                    = 22452
+        
+        done-with-keys                  = 22653
+        bail-out                        = 23443
+        
+        update-inv-arrows               = 26329
+        
+        return-the-object               = 26990
+        
+        scan-screen                     = 27337
+        check-give                      = 27566
+        update-selected                 = 27614
+        next-scan                       = 27804
+        
+        skip-logo-fade                  = 28522
+        
+    }
+    
+    college-office {
+        skip-credits                    = 55344
+    }
+    
+    desert-over {
+        vent-hydrogen                   = 102718
+        drop-ballast                    = 102760
+    }
+    
+    new-york-street {
+        clean-up-after-big-override     = 63460
+    
+        main-node                       = 64774
+        skip-introduction               = 65315
+        not-that-kind-of-show-node      = 66815
+        dont-kid-around-pal             = 67963
+        called-biff-ape-node            = 68285
+        calling-me-stupid-node          = 68575
+        apologize-node                  = 68925
+        talking-about-my-idol-node      = 69819
+        something-about-her-node        = 70179
+        the-way-she-node                = 70858
+        okay-for-a-college-boy          = 71676
+        angered-biff                    = 71686
+        good-night-node                 = 71724
+        
+        skip-climbing                   = 75055
+        skip-dialog                     = 75744
+        
+        skip-hide-and-seek              = 76616
+        
+        pos-changed                     = 77614
+        update-palette                  = 77725
+        no-change                       = 77747
+        
+        knockout-failed                 = 78714
+        opponent-hit-blocked            = 78948
+        opponent-hit-lands              = 78972
+        indy-hit-blocked                = 79054
+        indy-hit-lands                  = 79083
+        
+        handle-step-back                = 80254
+        choose-action-again             = 80547
+        attack                          = 80636
+        opponent-blocks                 = 81613
+        skip-game-over                  = 85710
+        finish-fight                    = 86379
+    }
+
+    sea-over {
+        vent-hydrogen                   = 77610
+        drop-ballast                    = 77656
+    }
+    
+}
+
+charsets {
+    system-font         = 1
+    fat-font            = 2
+    small-credits-font  = 3
+}
